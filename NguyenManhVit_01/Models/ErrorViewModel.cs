f556c04cb195b780e7d3d00af4ac923c4ba82950
DQT <dinhquynhtrang5@gmail.com>
2023-01-02 23:06:19 +0700

    Add project files.

---
+++ NguyenManhVit_01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenManhVit_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
